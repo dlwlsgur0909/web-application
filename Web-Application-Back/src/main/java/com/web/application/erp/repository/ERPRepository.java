57fc61644a4ff1585a5da3486f993a5abc908a55
LJH09 <dlwlsgur0909@naver.com>
2024-05-05 16:11:49 +0900

    fix: deleted legacy files

--- Web-Application-Back/src/main/java/com/web/application/erp/repository/ERPRepository.java
+++
@@ -1,7 +0,0 @@
-package com.web.application.erp.repository;
-
-import com.web.application.user.entity.UserProject;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface ERPRepository extends ListCrudRepository<UserProject, Long> {
-}
